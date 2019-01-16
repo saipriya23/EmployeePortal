1588fe4c73ff6a2b1ac5513870e6edb180ee1549
saipriya23 <saipriyareddy2307@gmail.com>
2019-01-16 18:49:06 +0530

    Employee

---
+++ EmployeePortal/Controllers/EmployeeFormController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeePortal.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeePortal.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class EmployeeFormController : ControllerBase
+    {
+        List<EmployeeList> employee = new List<EmployeeList>()
+        {
+            new EmployeeList{Id=1,EmployeeName="John",Designation="software engineer",Experience=2},
+            new EmployeeList{Id=2,EmployeeName="Mike",Designation="senior software engineer",Experience=4},
+            new EmployeeList{Id=3,EmployeeName="Sam",Designation=" software engineer",Experience=2},
+            new EmployeeList{Id=4,EmployeeName="Kiran",Designation="Technical Lead",Experience=10},
+            new EmployeeList{Id=5,EmployeeName="Arun",Designation="senior software engineer",Experience=5},
+        };
+        [HttpGet]
+        [Route("GetEmployees")]
+        public List<EmployeeList> GetEmployees()
+        {
+            return employee;
+        }
+        [Route("GetEmployee")]
+         public List<EmployeeList>  GetEmployee([FromBody] EmployeeList value)
+         {
+            employee.Add(value);
+            return employee;
+         }
+    }
+}
---
+++ EmployeePortal/Models/EmployeeList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeePortal.Models
+{
+    public class EmployeeList
+    {
+        public int Id { get; set; }
+        public string EmployeeName { get; set; }
+        public string Designation { get; set; }
+        public int Experience { get; set; }
+    }
+}
